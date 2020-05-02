c382c4b3d83afacb9d8671515c434660a8087507
Didier Fradet <didier.fradet@univ-lo>
2020-05-02 13:33:23 +0200

    Commit TP1 Exercice 2

---
+++ src/m2207/pratique/tp1/MainExercice2.java
@@ -0,0 +1,16 @@
+package m2207.pratique.tp1;
+
+/**
+ * Created by Didier Fradet on 01/05/2020.
+ */
+public class MainExercice2 {
+    public static void main(String[] args) {
+        for (int i = 3; i < 9; i++) {
+            System.out.println("le carré de " + i +" est : " + i*i);
+            /*
+            Ou bien
+            System.out.print(i*i + " ");
+            */
+        }
+    }
+}
